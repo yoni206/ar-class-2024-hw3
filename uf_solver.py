dc5cc319be3d4d89b7443330061d11ddab5be06e
Yoni Zohar <Have.one>
2022-04-09 23:44:50 +0300

    code and benchmarks

---
+++ uf_solver.py
@@ -0,0 +1,39 @@
+# importing system module for reading files
+import sys
+
+# import utility functions
+from utils import get_terms, is_flat_cube, get_function_symbols
+
+# import classses for parsing smt2 files
+from pysmt.smtlib.parser import SmtLibParser
+from six.moves import cStringIO
+
+# import pysmt functions for creating formulas and terms
+from pysmt.shortcuts import Not, Equals, Function
+
+# solver for flat cubes
+# returns True if flat_cube is satisfiable. 
+# Otherwise, returns False
+def flat_cube_uf_solver(flat_cube):
+  assert is_flat_cube(flat_cube)
+  return True
+
+# main function
+def main():
+  # read path from input
+  path = sys.argv[1]
+  with open(path, "r") as f:
+    smtlib = f.read()
+  
+  # parse the smtlib file
+  parser = SmtLibParser()
+  script = parser.get_script(cStringIO(smtlib))
+  formula = script.get_last_formula()
+  cube = formula.args()
+
+  # check if sat or unsat and print result
+  sat = flat_cube_uf_solver(cube)
+  print("sat" if sat else "unsat")
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,87 @@
+# importing classes from pysmt for utility functions
+from pysmt.walkers import IdentityDagWalker
+from pysmt.walkers.generic import handles
+import pysmt.operators as op
+from pysmt.shortcuts import get_env
+from pysmt.shortcuts import And
+
+# helper class
+class SubTermsGetter(IdentityDagWalker):
+  def __init__(self, env):
+    IdentityDagWalker.__init__(self, env=env, invalidate_memoization=True)
+    self.sub_terms = set([])
+  
+  @handles(set(op.ALL_TYPES))
+  def walk_collect(self, formula, args, **kwargs):
+    self.sub_terms.add(formula)
+
+# helper class
+class FunctionSymbolsGetter(IdentityDagWalker):
+  def __init__(self, env):
+    IdentityDagWalker.__init__(self, env=env, invalidate_memoization=True)
+    self.funs = set([])
+  
+  def walk_function(self, formula, args, **kwargs):
+    function_name = formula.function_name()
+    self.funs.add(function_name)
+  
+  @handles(set(op.ALL_TYPES) - set([op.FUNCTION]))
+  def default(self, formula, args, **kwargs):
+    return formula
+
+
+# Utility Functions
+
+# get all terms in a cube.
+# for example: get_terms([x=y, f(x)=z]) = [x, y, z, f(x)]
+def get_terms(cube):
+  formula = And(cube)
+  sub_terms_getter = SubTermsGetter(get_env())
+  sub_terms_getter.walk(formula)
+  return [t for t in sub_terms_getter.sub_terms if t.is_symbol() or t.is_function_application()]
+
+# get all function symbols in a cube.
+# for example: get_function_symbols([x=y, f(x)=z]) = [z]
+def get_function_symbols(cube):
+  formula = And(cube)
+  function_symbols_getter = FunctionSymbolsGetter(get_env())
+  function_symbols_getter.walk(formula)
+  return function_symbols_getter.funs
+
+def is_cube(cube):
+  for lit in cube:
+    if not is_lit(lit):
+      return False
+  return True
+
+def is_lit(term):
+  return term.is_equals() or (term.is_not() and term.args()[0].is_equals())
+
+def is_flat_lit(lit):
+  assert is_lit(lit)
+  if lit.is_equals():
+    left = lit.args()[0]
+    right = lit.args()[1]
+    if not left.is_symbol():
+      return False
+    if right.is_symbol():
+      return True
+    assert right.is_function_application()
+    if right.args()[0].is_symbol():
+      return True
+    else:
+      return False
+  else:
+    assert lit.is_not() and lit.args()[0].is_equals()
+    eq = lit.args()[0]
+    left = eq.args()[0]
+    right = eq.args()[1]
+    return left.is_symbol() and right.is_symbol()
+
+def is_flat_cube(cube):
+  if not is_cube(cube):
+    return False
+  for lit in cube:
+    if not is_flat_lit(lit):
+      return False
+  return True
